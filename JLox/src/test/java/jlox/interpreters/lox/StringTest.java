e49ceb67fc767e20bfbd30b64fa76a8263c3ac42
jjcard <jjcard@fakeemail.com>
2019-09-24 20:20:48 -0700

    add string tests from the suite

---
+++ JLox/src/test/java/jlox/interpreters/lox/StringTest.java
@@ -0,0 +1,39 @@
+package jlox.interpreters.lox;
+
+import static jlox.interpreters.lox.LoxTestUtil.assertLinesEquals;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+class StringTest extends BaseLoxTest {
+    
+    private static final String STRING_TEST_FILE_DIR = TEST_FILE_DIR + "string/";
+
+    
+    //*****From Test Suite
+    @Test
+    void errorAfterMultiline() throws IOException {
+        Lox.runFile(STRING_TEST_FILE_DIR + "error_after_multiline.lox");
+        LoxTestUtil.assertRuntimeError("Undefined variable 'err'.", 7);
+    }
+    @Test
+    void literals() throws IOException {
+        Lox.runFile(STRING_TEST_FILE_DIR + "literals.lox");
+        LoxTestUtil.assertHasNoErrors();
+        assertLinesEquals("()", "a string", "A~¶Þॐஃ");
+    }
+    @Test
+    void multiline() throws IOException {
+        Lox.runFile(STRING_TEST_FILE_DIR + "multiline.lox");
+        LoxTestUtil.assertHasNoErrors();
+        assertLinesEquals("1\n" + 
+                "2\n" + 
+                "3");
+    }
+    
+    @Test
+    void unterminated() throws IOException {
+        Lox.runFile(STRING_TEST_FILE_DIR + "unterminated.lox");
+        LoxTestUtil.assertErrorLineEquals("[line 2] Error: Unterminated string.");
+    }
+}
