099fa58213e1a4c029811053fbd4e65cb85556a1
jjcard <jjcard@fakeemail.com>
2021-11-28 13:13:07 -0800

    finish chapter 19

--- CLox/CLox/memory.h
+++ CLox/CLox/memory.h
@@ -1,6 +1,14 @@
 #ifndef clox_memory_h                    
 #define clox_memory_h 
 
+#include "common.h"
+#include "object.h"
+
+#define ALLOCATE(type, count) \
+    (type*)reallocate(NULL, 0, sizeof(type) * (count))
+
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
+
 #define GROW_CAPACITY(capacity) \
 ((capacity) < 8 ? 8 : (capacity) * 2)
 
@@ -14,5 +22,6 @@
 
 void* reallocate(void* previous, size_t oldSize, size_t newSize);
 
+void freeObjects();
 
 #endif
